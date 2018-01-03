2e18cc70170ef8d8909f910e8a4a8de738f55c5a
Dominik Rubo <dominik.rubo@gmail.com>
2018-01-03 21:07:05 +0100

    install_requirements: Show final message only on success

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 echo "Installing required Python packages"
-pip3 install --upgrade --requirement requirements.txt
+pip3 install --upgrade --requirement requirements.txt &&
 echo "All required Python packages installed"
